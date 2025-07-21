c4c583b479b4b9773cb047c09d18d09152425825
shreelaxmikamath <shreelaxmi.k111@gmail.com>
2025-07-21 19:36:16 +0530

    fav-jokes props datatypes

--- fav-jokes/src/App.jsx
+++ fav-jokes/src/App.jsx
@@ -6,18 +6,34 @@ function App() {
     <>
       <Joke 
           punchline="I can't wait to see her face light up when she opens it."
+          upvotes={10}
+          downvotes={5}
+          comments={["eww","cool"]}
+          ispun={!true}
       />
       <Joke 
           setup="How did the hacker escape the police"
           punchline="He just ransomware!"
+          upvotes={0}
+          downvotes={5}
+          comments={["eww","eeks"]}
+          ispun={true}
       />
       <Joke 
           setup="Why don't pirates travel on mountain roads?"
           punchline="Scurvy."
+          upvotes={30}
+          downvotes={50}
+          comments={["eww","sus"]}
+          ispun={true}
       />
       <Joke 
           setup="Why do bees stay in the hive in the winter?"
           punchline="Swarns"
+          upvotes={1}
+          downvotes={5}
+          comments={["eww","Nahh!"]}
+          ispun={true}
       />
     </>
   )
--- fav-jokes/src/components/Joke.jsx
+++ fav-jokes/src/components/Joke.jsx
@@ -1,9 +1,15 @@
 export default function Joke(props) {
+
     return(
         <>
         {props.setup && <p className="setup">Setup: {props.setup}</p>}
             <p className="punchline">Punchline: {props.punchline}</p>
-            <hr />
+            <p>upvotes:{props.upvotes} | downvotes :{props.downvotes} | Pun: {props.ispun?"yes":"no"}</p>
+            <ul>
+                {props.comments.map((comment, index) => (
+                    <li key={index}>{comment}</li>
+                ))}
+            </ul>            <hr />
         </>
     )
     
