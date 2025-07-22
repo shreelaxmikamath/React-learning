691cad9181178820bf86b0e2191f6deca1354ef4
shreelaxmikamath <shreelaxmi.k111@gmail.com>
2025-07-22 18:09:36 +0530

    props

--- travel_journal/src/components/Entry.jsx
+++ travel_journal/src/components/Entry.jsx
@@ -1,26 +1,22 @@
 import '../index.css'
-function Entry(params) {
+function Entry(props) {
     return(
         <article className="journal-entry">
             <div className='main-image-container'>
-              <img src="/japan-img.jpg" alt="Japan" className='main-image'/>
+              <img src={props.img} alt={props.alt} className='main-image'/>
 </div>
    
             <div className='info-container'>
                 <img src="/pin.png" alt="map marker icon" className='pinicon'/>
-                <span>Japan</span>
+                <span>{props.country}</span>
                 <a 
-                    href="https://www.google.com/maps/place/Mount+Fuji/@35.3606422,138.7186005,17z/data=!4m14!1m6!3m5!1s0x6019619629a6f294:0x889c6631fce556b2!8m2!3d35.3606422!4d138.7273634!16zL20vMGNrc2A?entry=ttu"
+                    href={props.googleMapsLink}
                 >
                     View on Google Maps
                 </a>
-                <h2 className="entry-title">Mount Fuji</h2>
-<h5 className="trip-dates">12 Jan, 2021 - 24 Jan, 2021</h5>
-<p className="entry-text">
-  Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). 
-  Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.
-  
-</p>
+                <h2 className="entry-title">{props.title}</h2>
+<h5 className="trip-dates">{props.dates}</h5>
+<p className="entry-text">{props.text}</p>
 
             </div>
 
--- travel_journal/src/components/Header.jsx
+++ travel_journal/src/components/Header.jsx
@@ -5,7 +5,7 @@ function Header(params) {
         <>
         <div className='header'>
             <div className='header-box'>    
-        <img src="/planet-earth.png" alt="globe" width="60px" height="70px"/>
+        <img src="/planet-earth.png" alt="globe" width="50px" height="50px"/>
         <h2 className='header-title' >My travel journal</h2>
             </div>
         </div>
