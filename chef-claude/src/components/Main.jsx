c9f95aa86bd60ebb2de310856092005deb7e7968
shreelaxmikamath <shreelaxmi.k111@gmail.com>
2025-07-27 11:01:32 +0530

    Forms

--- chef-claude/src/components/Main.jsx
+++ chef-claude/src/components/Main.jsx
@@ -3,21 +3,19 @@ import '../index.css'
 import { useState } from 'react'
 
 export default function Main(params) {
-const ingredients = ["Chicken", "Oregano", "Tomatoes"]
+const [ingredients,setIngredients] = useState([])
 
 const ingredientElements=ingredients.map((item)=>{
     return <li key={item}>{item}</li>
 })
-function handleSubmit(event) {
-    event.preventDefault()
-    const formData = new FormData(event.currentTarget);
+function handleSubmit(formData) {
     const newIngredient = formData.get("ingredient");
-    console.log("Form sub")
+    setIngredients(prevIngredients=>[...prevIngredients,newIngredient])
 }
 
     return(
         <main>
-            <form className="add-ingredient-form" onSubmit={handleSubmit}>
+            <form className="add-ingredient-form"  action={handleSubmit}>
                 <input 
                     type="text"
                     placeholder="e.g. oregano"
--- state/src/App.jsx
+++ state/src/App.jsx
@@ -143,34 +143,80 @@
  */
 
 export default function App() {
-  
   /**
-   * Challenge: get the password from the form too and log
-   * it to the console to be sure it came in correctly.
+   * Challenge: see if you can remember how to grab an array of checked
+   * items from the dietaryRestrictions checkboxes.
    */
-  
+
   function signUp(formData) {
-    const email = formData.get("email")
-    const password=formData.get("password")
-    console.log(password)
+    const data = Object.fromEntries(formData)
+    const dietaryData = formData.getAll("dietaryRestrictions" )
+    console.log(dietaryData)
   }
-  
+
   return (
     <section>
       <h1>Signup form</h1>
       <form action={signUp}>
+
         <label htmlFor="email">Email:</label>
-        <input id="email" type="email" name="email" placeholder="joe@schmoe.com" />
-        <br />
-        
+        <input id="email" defaultValue="joe@schmoe.com" type="email" name="email" placeholder="joe@schmoe.com" />
+
         <label htmlFor="password">Password:</label>
-        <input id="password" type="password" name="password" />
-        <br />
-        
+        <input id="password" defaultValue="password123" type="password" name="password" />
+
+        <label htmlFor="description">Description:</label>
+        <textarea id="description" name="description" defaultValue="This is a description"></textarea>
+
+        <fieldset>
+          <legend>Employment Status:</legend>
+          <label>
+            <input type="radio" name="employmentStatus" value="unemployed" />
+            Unemployed
+        </label>
+          <label>
+            <input type="radio" name="employmentStatus" value="part-time" />
+            Part-time
+        </label>
+          <label>
+            <input type="radio" name="employmentStatus" defaultChecked={true} value="full-time" />
+            Full-time
+        </label>
+        </fieldset>
+
+        <fieldset>
+          <legend>Dietary restrictions:</legend>
+          <label>
+            <input type="checkbox" name="dietaryRestrictions" value="kosher" />
+            Kosher
+        </label>
+          <label>
+            <input type="checkbox" name="dietaryRestrictions" value="vegan" />
+            Vegan
+        </label>
+          <label>
+            <input type="checkbox" name="dietaryRestrictions" defaultChecked={true} value="gluten-free" />
+            Gluten-free
+        </label>
+        </fieldset>
+
+        <label htmlFor="favColor">What is your favorite color?</label>
+        <select id="favColor" name="favColor" defaultValue="" required>
+          <option value="" disabled>-- Choose a color --</option>
+          <option value="red">Red</option>
+          <option value="orange">Orange</option>
+          <option value="yellow">Yellow</option>
+          <option value="green">Green</option>
+          <option value="blue">Blue</option>
+          <option value="indigo">Indigo</option>
+          <option value="violet">Violet</option>
+        </select>
+
         <button>Submit</button>
-        
+
       </form>
     </section>
   )
 }
+
 
